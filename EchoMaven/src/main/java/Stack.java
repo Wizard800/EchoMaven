ef1680413143abd3a4eb34db74d42185faa80aaf
ProDataMan <ProDataMan@outlook.com>
2017-12-04 14:05:43 -0800

    Added Stack Tests

--- EchoMaven/src/main/java/Stack.java
+++ EchoMaven/src/main/java/Stack.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
 
 public class Stack {
-
+ArrayList<String> stack;
+	public Stack() {
+		stack = new ArrayList<String>();
+	}
 	public Boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return true;
+		return stack.isEmpty();
 	}
+
+	public String push(String expected) {
+		// TODO Auto-generated method stub
+		if (stack.add(expected))
+			return expected;
+		else
+			return "";
+		
+	}
+	
 
 }
--- EchoMaven/src/test/java/StackTest.java
+++ EchoMaven/src/test/java/StackTest.java
@@ -9,5 +9,19 @@ public class StackTest {
 		Stack stack = new Stack();
 		assertTrue(stack.isEmpty());
 	}
-
+	@Test
+	public void pushTest() {
+		Stack stack = new Stack();
+		String expected = "Hello, World!";
+		String actual = stack.push(expected);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void pushIsEmptyTest() {
+		Stack stack = new Stack();
+		String expected = "Hello, World!";
+		String actual = stack.push(expected);
+		assertFalse(stack.isEmpty());
+	}
 }
